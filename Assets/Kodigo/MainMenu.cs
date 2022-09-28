444737f19af568fa5e84d07e540e53e4c135a450
Rayckens <andres.galeano8@misena.edu.co>
2022-09-28 11:09:18 -0500

    28/09/2022

--- Assets/Kodigo/MainMenu.cs
+++ Assets/Kodigo/MainMenu.cs
@@ -9,10 +9,12 @@ public class MainMenu : MonoBehaviour
 {
     public Image sound;
     public Sprite soundOn, soundOff;
+    public GameObject levelsMenu;
 
     
     private void Start()
     {
+        levelsMenu.SetActive(false);
         sound.sprite = OptionManager.music ? soundOff : soundOn;
         GetComponent<AudioSource>().mute = OptionManager.music;
     }
@@ -24,10 +26,37 @@ public class MainMenu : MonoBehaviour
     }
     public void PlayButton()
     {
-        SceneManager.LoadScene(1);
+        levelsMenu.SetActive(true);
     }
     public void ExitButton()
     {
         Application.Quit();
     }
+    public void MenuSelection(int seleccion)
+    {
+        switch (seleccion)
+        {
+            case 1:
+                SceneManager.LoadScene(2);
+                break;
+            case 2:
+                SceneManager.LoadScene(3);
+                break;
+            case 3:
+                SceneManager.LoadScene(4);
+                break;
+            case 4:
+                SceneManager.LoadScene(5);
+                break;
+            case 5:
+                SceneManager.LoadScene(6);
+                break;
+            case 0:
+                SceneManager.LoadScene(1);
+                break;
+            default:
+                break;
+        }
+    }
+    
 }
